90006d0d8226ad62c78ce67c36524f26b7349fb6
xps-zzq <496268609@qq.com>
2017-02-26 15:31:41 +0800

    单例模式实现

---
+++ src/main/Java/designmode/single/Singleton.java
@@ -0,0 +1,28 @@
+package designmode.single;
+
+/**
+ * Created by zzqno on 2017-2-26.
+ */
+public class Singleton {
+
+    private Singleton(){}
+
+
+    private static byte[] lock = new byte[0];
+    private static Singleton singleton;
+
+
+    public static Singleton getInstance(){
+        if(singleton == null){
+            synchronized (lock){
+                if(singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+
+        }
+        return singleton;
+    }
+
+
+}
