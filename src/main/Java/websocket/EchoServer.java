eb9d348f65dffbe253f226c31e38b27f0f811f6d
xps-zzq <496268609@qq.com>
2017-03-12 13:21:25 +0800

    websocket 服务端实现

--- src/main/Java/websocket/EchoServer.java
+++ src/main/Java/websocket/EchoServer.java
@@ -1,6 +1,8 @@
 package websocket;
 
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 /**
@@ -11,7 +13,14 @@ import javax.websocket.server.ServerEndpoint;
 public class EchoServer {
 
     @OnMessage
-    public String echo(String incomingMessage){
-        return "I got this("+incomingMessage+")"+" so I am sending it back!";
+    public String echo(String incomingMessage) {
+        return "I got this(" + incomingMessage + ")" + " so I am sending it back!";
     }
+
+    @OnOpen
+    public void init(Session session){
+        System.out.println(" on open");
+    }
+
+
 }
