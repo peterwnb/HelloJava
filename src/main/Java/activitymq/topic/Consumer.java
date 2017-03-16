0684385f7c534104d5eb381975dd383e29e7fb06
xps-zzq <496268609@qq.com>
2017-03-16 21:25:25 +0800

    activemq demo

---
+++ src/main/Java/activitymq/topic/Consumer.java
@@ -0,0 +1,48 @@
+package activitymq.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Created by zzqno on 2017-3-15.
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws JMSException {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://192.168.159.128:61616");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        /**
+         Session javax.jms.Connection.createSession(boolean transacted, int acknowledgeMode) throws JMSException
+         1.transacted事务，事务成功commit,才会将消息发送到mom中
+         2.acknowledgeMode消息确认机制
+         1）、带事务的session
+         如果session带有事务，并且事务成功提交，则消息被自动签收。如果事务回滚，则消息会被再次传送。
+         2）、不带事务的session
+         不带事务的session的签收方式，取决于session的配置。
+         Activemq支持一下三種模式：
+         Session.AUTO_ACKNOWLEDGE  消息自动签收
+         Session.CLIENT_ACKNOWLEDGE  客戶端调用acknowledge方法手动签收
+         Session.DUPS_OK_ACKNOWLEDGE 不是必须签收，消息可能会重复发送。在第二次重新传送消息的时候，消息
+         头的JmsDelivered会被置为true标示当前消息已经传送过一次，客户端需要进行消息的重复处理控制。
+         代码示例如下：
+         session = connection.createSession(true, Session.CLIENT_ACKNOWLEDGE);
+         textMsg.acknowledge();
+         */
+        Topic topic = session.createTopic("XPS-15-zzq.topic");
+
+        MessageProducer producer = session.createProducer(topic);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);//设置非持久化
+//          producer.setTimeToLive(5000);//5秒后过期，这个对点对点模式有效
+        TextMessage message = session.createTextMessage();
+        message.setText("message_" + System.currentTimeMillis());
+        producer.send(message);
+        System.out.println("Sent message: " + message.getText());
+        session.close();
+        connection.stop();
+        connection.close();
+    }
+}
---
+++ src/main/Java/activitymq/topic/Producer.java
@@ -0,0 +1,59 @@
+package activitymq.topic;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Created by zzqno on 2017-3-15.
+ */
+public class Producer {
+
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // 默认的连接用户名
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // 默认的连接密码
+    private static final String BROKEURL = "tcp://192.168.159.128:61616"; // 默认的连接地址
+    private static final int SENDNUM = 10; // 发送的消息数量
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory; // 连接工厂
+        Connection connection = null; // 连接
+        Session session; // 会话 接受或者发送消息的线程
+        Destination destination; // 消息的目的地
+        MessageProducer messageProducer; // 消息生产者
+
+        // 实例化连接工厂
+        connectionFactory = new ActiveMQConnectionFactory(Producer.USERNAME, Producer.PASSWORD, Producer.BROKEURL);
+
+        try {
+            connection = connectionFactory.createConnection(); // 通过连接工厂获取连接
+            connection.setClientID("XPS-15-zzq");
+            connection.start(); // 启动连接
+            session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE); // 创建Session,第一个参数为是否开启事务
+            Topic topic = session.createTopic("XPS-15-zzq.topic");
+            TopicSubscriber subs = session.createDurableSubscriber(topic, "XPS-15-zzq.topic");
+            subs.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    TextMessage tm = (TextMessage) message;
+                    try {
+                        System.out.println("Received message: " + tm.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
