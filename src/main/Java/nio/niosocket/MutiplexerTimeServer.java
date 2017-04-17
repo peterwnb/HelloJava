92b296dad550a304010a006bb0bfb16589f6ab45
xps-zzq <496268609@qq.com>
2017-04-17 22:36:38 +0800

    nio server demo

--- src/main/Java/nio/niosocket/MutiplexerTimeServer.java
+++ src/main/Java/nio/niosocket/MutiplexerTimeServer.java
@@ -11,7 +11,19 @@ import java.util.Set;
 /**
  * Created by zzqno on 2017-4-12.
  * 服务端
- *
+ * <p>
+ *  NIO编程步骤
+ * 1.打开ServerSocketChannel，监听客户端连接
+ * 2.绑定监听端口，设置连接为非阻塞模式
+ * 3. 创建Reactor线程，创建多路复用器并启动线程
+ * 4.将ServerSocketChannel注册到Reactor线程中的Selector上，监听ACCEPT事件
+ * 5.Selector轮询准备就绪的key
+ * 6.Selector监听到新的客户端接入，处理新的接入请求，完成TCP三次握手，简历物理链路
+ * 7.设置客户端链路为非阻塞模式
+ * 8.将新接入的客户端连接注册到Reactor线程的Selector上，监听读操作，读取客户端发送的网络消息
+ * 9.异步读取客户端消息到缓冲区
+ * 10.对Buffer编解码，处理半包消息，将解码成功的消息封装成Task
+ * 11.将应答消息编码为Buffer，调用SocketChannel的write将消息异步发送给客户端
  */
 public class MutiplexerTimeServer implements Runnable {
 
@@ -109,7 +121,7 @@ public class MutiplexerTimeServer implements Runnable {
                     String body = new String(bytes, "UTF-8");
                     System.out.println("The time server receive order:" + body);
                     String currTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
-                    doWrite(socketChannel,currTime);
+                    doWrite(socketChannel, currTime);
                 } else if (readBytes < 0) {
                     key.cancel();
                     socketChannel.close();
